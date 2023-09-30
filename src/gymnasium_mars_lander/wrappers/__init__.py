e466608d2647132ed0e21702f0f877603fda67cc
Quentin18 <quentindeschamps18@gmail.com>
2023-09-30 11:12:31 +0200

    add negative reward wrapper

--- src/gymnasium_mars_lander/wrappers/__init__.py
+++ src/gymnasium_mars_lander/wrappers/__init__.py
@@ -1,3 +1,4 @@
 from gymnasium_mars_lander.wrappers.euclidean_distance_reward import (
     EuclideanDistanceRewardWrapper,
 )
+from gymnasium_mars_lander.wrappers.negative_reward import NegativeRewardWrapper
