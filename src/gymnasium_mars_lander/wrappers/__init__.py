f8b91877f3544f59c7b2d48be97cc601338ed6dc
Quentin18 <quentindeschamps18@gmail.com>
2026-05-09 00:14:08 +0200

    chore: remove unused wrappers

--- src/gymnasium_mars_lander/wrappers/__init__.py
+++
@@ -1,4 +0,0 @@
-from gymnasium_mars_lander.wrappers.euclidean_distance_reward import (
-    EuclideanDistanceRewardWrapper,
-)
-from gymnasium_mars_lander.wrappers.negative_reward import NegativeRewardWrapper
